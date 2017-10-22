549658da3cc524ed8fba115c50a8711bd93d735a
Mauricio Piñon Islas <mauricio_2612@live.com.mx>
2017-10-21 23:11:30 -0500

    Se crea angular app con una integración de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+
+  coworking: Observable<any[]>;
+
+  constructor(public afDB: AngularFireDatabase, private authorizationService: AuthService) {
+    this.coworking = afDB.list('Cocow').valueChanges();
+  }
+
+
+
+
+
+
+
+  login() {
+    this.authorizationService.login()
+      .then((data) => {
+        console.log(data);
+        console.log(data.user);
+        console.log(data.user.displayName)
+        var userName = data.user.displayName
+        var userMail= data.user.email
+        this.createCW(userName,userMail);
+        alert('Loggeado exitosamente');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Hubo un error al loggearte');
+      })
+  }
+  createCW(userName, userMail){
+    this.afDB.database.ref('cocow/users/').set({
+    username: userName,
+    email: userMail,
+    // profile_picture : imageUrl
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './services/auth.service';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAN3mTzAzadd3FzAGi9cVXJ0NkUsqpAmaU",
+  authDomain: "cocow-79a36.firebaseapp.com",
+  databaseURL: "https://cocow-79a36.firebaseio.com",
+  storageBucket: "cocow-79a36.appspot.com",
+  messagingSenderId: "179020787791"
+};
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
